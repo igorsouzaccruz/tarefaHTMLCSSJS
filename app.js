726fa72f763a8ebf3053b284a08885106566624a
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-09-30 11:59:52 -0300

    Refatorando CSS e Quest01

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ function conversaoABNT(nomeCompleto) {
   let ultimoNome = nomesNoArray[nomesNoArray.length - 1];
   let primeiraLetraDosNomesNoArray = [];
 
-  nomeConvertido = ultimoNome + ", " + primeiroNome;
+  if(nomesNoArray.length > 1){
+    nomeConvertido = ultimoNome + ", " + primeiroNome;
+  }else{
+    nomeConvertido = ultimoNome;
+  }
+  
 
   for (let nome = 1; nome < nomesNoArray.length - 1; nome++) {
     primeiraLetraDosNomesNoArray.push(nomesNoArray[nome].charAt(0));
@@ -25,4 +30,6 @@ const getValueInput = () => {
   inputNomeQuestao01 = document.getElementById("nomeq1").value;
   let respostaQuestao01 = conversaoABNT(inputNomeQuestao01);
   document.getElementById("resposta01").innerHTML = respostaQuestao01;
+  document.getElementById("resposta01").style.background = 'white';
+  document.getElementById("resposta01").style.color = '#006400';
 };
