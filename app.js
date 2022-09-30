43257b51dc644699a8a21d35bfcfc5b054056d8e
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-09-29 21:06:06 -0300

    separando array nome

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const getValueInput = () =>{
     nomeq1 = document.getElementById("nomeq1").value; 
     document.getElementById("resposta01").innerHTML = nomeq1;
     let nomes = nomeq1.trim().split(/\s+/);
+    let ultimoNome = nomes[nomes.length -1];
     console.log(nomes);
+    let primeiraLetraNomeNoArray = [];
+    for (let nome = 0; nome < nomes.length - 1; nome++) {
+      primeiraLetraNomeNoArray.push(nomes[nome].charAt(0)); 
+    }
+    console.log(ultimoNome);
+    console.log(primeiraLetraNomeNoArray);
   }
   
